dab7f2543ca13de4131357f85349153dd9484360
Megan Frenkel <38475288+mmfrenkel@users.noreply.github.com>
2020-12-06 15:46:46 -0500

    Travis (#22)
    * Preliminary travis file specifications
    
    * Changed language to minimal for now
    
    * Updated run_backend_tests to be more generic
    
    * Updated backend test a bit more
    
    * Added bin to backend dockerfile, attempt tests
    
    * Attempt to make backend docker use bin
    
    * Fixed error in Dockerfile
    
    * Trying absolute paths for script
    
    * Misunderstood docker run vs exec, trying exec
    
    * Update travis script command
    
    * Preliminary travis file specifications
    
    * Changed language to minimal for now
    
    * Updated run_backend_tests to be more generic
    
    * Updated backend test a bit more
    
    * Added bin to backend dockerfile, attempt tests
    
    * Attempt to make backend docker use bin
    
    * Fixed error in Dockerfile
    
    * Trying absolute paths for script
    
    * Misunderstood docker run vs exec, trying exec
    
    * Update travis script command
    
    * update report directories, travis run front end tests
    
    * remove reports duplicated at root
    
    * add reports/bin directory server and app respectively
    
    * add frontend test to auto-test suite
    
    * attempt to fix build
    
    * Travis uses sh script for backend tests
    
    * attempt to save files from docker container to repo
    
    * refactoring travis_push
    
    * attempt to use github pages
    
    * return to old approach
    
    * update travis push
    
    * another attempt... sigh
    
    * another attempt... sigh
    
    * quick change; unsure why travis didnt run
    
    * fix reformatted command
    
    * add pull before commit to update branch
    
    Co-authored-by: Kundan Guha <kundan.guha@columbia.edu>

---
+++ .travis_push.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout -b travis_results
+  git pull origin travis_results
+  git add travis_reports 
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "Commited test files..."
+}
+
+upload_files() {
+  echo "Setting remote origin..."
+  git remote set-url origin https://mmfrenkel:${GH_TOKEN}@github.com/mmfrenkel/KERMit.git > /dev/null 2>&1
+  
+  echo "Pushing travis CI results to github..."
+  git push --quiet --set-upstream origin travis_results
+}
+
+setup_git
+commit_files
+upload_files
+
---
+++ server/bin/run_backend_bugs_style_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+filename="pylint-$current_time.txt"
+
+echo "Running pylint for source files..."
+pylint . --ignore venv --ignore tests \
+  --load-plugins pylint_flask_sqlalchemy \
+  --disable cyclic-import | tee ./reports/style_bug_checker/$filename
+
+echo "Running pylint errors for test files..."
+pylint ./tests --load-plugins pylint_flask_sqlalchemy \
+  --disable redefined-outer-name \
+  --disable unused-argument \
+  --disable unused-import \
+  --disable no-self-use \
+  --disable too-few-public-methods \
+  --disable R0801 | tee ./reports/backend/style_bug_checker/tests-$filename
