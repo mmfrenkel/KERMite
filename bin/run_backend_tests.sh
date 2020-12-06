d6256bcfbd5e631261863f8190b566cad0eeb86e
Kundan Guha <kundan.guha@columbia.edu>
2020-12-06 11:26:51 -0500

    Updated run_backend_tests to be more generic

--- bin/run_backend_tests.sh
+++ bin/run_backend_tests.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-	then 
-		run_type="all"
-	else
-		run_type="$1"
-fi 
+run_type_d="all"
+test_dir_d="./server/tests"
+report_dir_d="./reports/backend/tests"
 
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+if [ "-h" = "$1" ] || [ "--help" = "$1" ]; then
+    echo "Usage: $0 [run_type] [test_dir] [report_dir]"
+    echo "Defaults: run_type = $run_type_d, test_dir = $test_dir_d, report_dir = $report_dir_d"
+    exit
+fi
+
+run_type="${1:-$run_type_d}"
+test_dir="${2:-$test_dir_d}"
+report_dir="${3:-$report_dir_d}"
+
+current_time="$(date --utc "+%FT%TZ")"
 filename="$run_type-$current_time.txt"
 
-python -m pytest -v ./server/tests/$1 2>&1 | tee ./reports/backend/tests/$filename 
-
+python -m pytest -v "$test_dir/$1" 2>&1 | tee "$report_dir/$filename"
