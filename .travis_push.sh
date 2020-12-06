bf9e00357fd97e8808b82bdca215de2d8aa363cc
mmfrenkel <megan.frenkel@gmail.com>
2020-12-06 16:51:45 -0500

    retry pull approach

--- .travis_push.sh
+++ .travis_push.sh
@@ -6,18 +6,17 @@ setup_git() {
 }
 
 commit_files() {
+
+  echo "Setting remote origin..."
+  git remote set-url origin https://mmfrenkel:${GH_TOKEN}@github.com/mmfrenkel/KERMit.git > /dev/null 2>&1
+
   git checkout -b travis_results
-  git add travis_reports
   git stash
   git pull origin travis_results 
   git stash pop
+  git add travis_reports
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   echo "Commited test files..."
-}
-
-upload_files() {
-  echo "Setting remote origin..."
-  git remote set-url origin https://mmfrenkel:${GH_TOKEN}@github.com/mmfrenkel/KERMit.git > /dev/null 2>&1
   
   echo "Pushing travis CI results to github..."
   git push --quiet --set-upstream origin travis_results
@@ -25,5 +24,4 @@ upload_files() {
 
 setup_git
 commit_files
-upload_files
 
