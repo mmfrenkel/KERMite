194d928b54e791ea3d0935c446caeb7932514121
mmfrenkel <megan.frenkel@gmail.com>
2020-12-06 15:04:32 -0500

    update travis push

--- .travis_push.sh
+++ .travis_push.sh
@@ -5,18 +5,22 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
+commit_files() {
   git checkout -b travis_results
-  git add backend_test_results frontend_test_results
+  git add travis_reports 
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "Commited test files..."
 }
 
 upload_files() {
+  echo "Setting remote origin..."
   git remote add origin https://${GH_TOKEN}@github.com/mmfrenkel/KERMit.git > /dev/null 2>&1
+  
+  echo "Pushing travis CI results to github"
   git push --quiet --set-upstream origin travis_results
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
 
