685d20fa00717f64391c50317c0c9ba2cf320b33
Kundan Guha <kundan.guha@columbia.edu>
2020-12-06 18:04:03 -0500

    Modified travis to trigger on nightly branch too

--- .travis_push.sh
+++ .travis_push.sh
@@ -1,37 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+git_show_current() {
+    local branch
+    branch="$(git branch --show-current 2>/dev/null)"
+    # shellcheck disable=SC2181
+    if [ $? -ne 0 ];then
+        branch="$(git rev-parse --abbrev-ref HEAD)"
+    fi
+    echo "$branch"
+    return 0
 }
 
 on_branch() {
-    local branch="$(git branch --show-current)"
-    if [ "$1" = "$branch" ];then
-        return 0
-    fi
+    local branch
+    branch="$(git_show_current)"
+    [ "$1" = "$branch" ] && return 0
+    return 1
+}
+
+check_branches() {
+    local branches=(main nightly)
+    for branch in "${branches[@]}";do
+        on_branch "$branch" && return 0
+    done
     return 1
 }
 
 commit_files() {
 
-  echo "Setting remote origin..."
-  git remote set-url origin https://mmfrenkel:${GH_TOKEN}@github.com/mmfrenkel/KERMit.git > /dev/null 2>&1
+    echo "Setting remote origin..."
+    git remote set-url origin "https://mmfrenkel:${GH_TOKEN}@github.com/mmfrenkel/KERMit.git" > /dev/null 2>&1
 
-  git checkout -b travis_results
-  git add travis_reports
-  git stash
-  git pull origin travis_results
-  git stash pop
-  git add travis_reports
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  echo "Commited test files..."
+    git checkout -b travis_results
+    git add travis_reports
+    git stash
+    git pull origin travis_results
+    git stash pop
+    git add travis_reports
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    echo "Commited test files..."
 
-  echo "Pushing travis CI results to github..."
-  git push --quiet --set-upstream origin travis_results
+    echo "Pushing travis CI results to github..."
+    git push --quiet --set-upstream origin travis_results
 }
 
 setup_git
-if on_branch main;then
+if check_branches;then
     commit_files
 fi
